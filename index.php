59d1d9497d9e43153e2e0100d48c7249ae62d541
Eduardo Cardoso <144170650+EduardoCardoso14@users.noreply.github.com>
2024-01-01 22:22:47 -0300

    adicionei os primeiros arquivos 01.01.24

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Portfólio - Eduardo Cardoso</title>
+</head>
+
+<body>
+    <header>
+        <nav>
+            <span class="links">
+                <a href="#">Sobre mim</a>
+                |
+                <a href="#">Contatos</a>
+            </span>
+        </nav>
+    </header>
+    <main>
+        <div class="container-main">
+            <div class="circulo">
+                <img src="img/eukk.jpeg" alt="">
+            </div>
+        </div>
+    </main>
+    <footer>
+        <span class="principalFooter">
+            <h3>Eduardo Cardoso © Todos os direitos reservados.</h3>
+        </span>
+        <span class="secondFooter">
+            <h4>
+                <label for="">Contatos (Clique para contatar):</label>
+                <span class="listaContato">
+                    <ul>
+                        <li>
+                            <img src="img/instagram.png" alt="">
+                            <a href="">Instagram</a>
+                        </li>
+                        <li>
+                            <img src="img/whatsapp.png" alt="">
+                            <a href="">WhatsApp</a>
+                        </li>
+                        <li>
+                            <img src="img/linkedin.png" alt="">
+                            <a href="">Linkedln</a>
+                        </li>
+                    </ul>
+                </span>
+            </h4>
+        </span>
+    </footer>
+</body>
+
+</html>
